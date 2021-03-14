26b58e87061d8cb02e029b28b4ca883ed5c51b50
DESKTOP-844MEAT\husey <huseyin.sade@ogr.sakarya.edu.tr>
2021-03-14 10:09:51 +0300

    sade

---
+++ ApartFinder.DataAccess/ApartDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApartFinder.DataAccess
+{
+    public class ApartDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=DESKTOP-844MEAT; Database=ApartDb;");
+        }
+    }
+}
